671bc5677452f9c04f78cb4b13ae51b7d8c162a0
Savas Aydin <esavayd@esekiws5153.rnd.ki.sw.ericsson.se>
2013-02-20 08:03:01 +0100

    game_server application to start and stop

--- src/game_server_app.erl
+++ src/game_server_app.erl
@@ -1,16 +1,9 @@
 -module(game_server_app).
-
 -behaviour(application).
-
-%% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start(_StartType, _StartArgs) ->
+start(normal, []) ->
     game_server_sup:start_link().
 
-stop(_State) ->
-    ok.
+stop(_) -> ok.
+
