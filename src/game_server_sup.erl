317f7b1bb48337c7623aa1ad8c149d7ba944ca52
Savas Aydin <esavayd@esekiws5153.rnd.ki.sw.ericsson.se>
2013-02-20 08:02:01 +0100

    listens port and spawns new worker

--- src/game_server_sup.erl
+++ src/game_server_sup.erl
@@ -1,28 +1,29 @@
-
 -module(game_server_sup).
-
 -behaviour(supervisor).
 
-%% API
--export([start_link/0]).
+-export([start_link/0,
+	 start_socket/0,
+	 init/1
+	]).
 
-%% Supervisor callbacks
--export([init/1]).
-
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
+-define(PORT, 8080).
+-defıne(TCP_OPTIONS, [{active, once}, {packet, 0}]).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
-
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, ListenSocket} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
+    spawn_link(fun available_listeners/0),
+    {ok, {{simple_one_for_one, 60, 3600},
+	  [{socket,
+	    {game_server, start_link, [ListenSocket]},
+	    temporary, 1000, worker, [game_server]}
+	  ]}}.
 
+start_socket() ->
+    supervisor:start_child(?MODULE, []).
+ 
+available_listeners() ->
+    [start_socket() || _ <- lists:seq(1,20)],
+    ok.
