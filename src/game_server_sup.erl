8aaf4d78de0827adba86b42973e2db0698ccc0e1
Savas Aydin <esavayd@esekiws5153.rnd.ki.sw.ericsson.se>
2013-03-04 19:30:29 +0100

    changed restart strategy and removed additional listeners

--- src/game_server_sup.erl
+++ src/game_server_sup.erl
@@ -6,24 +6,16 @@
 	 init/1
 	]).
 
--define(PORT, 8080).
--define(TCP_OPTIONS, [{active, once}, {packet, 0}]).
-
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    {ok, ListenSocket} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
-    spawn_link(fun available_listeners/0),
-    {ok, {{simple_one_for_one, 60, 3600},
-	  [{socket,
-	    {game_server, start_link, [ListenSocket]},
+    {ok, {{one_for_one, 60, 3600},
+	  [{game_server,
+	    {game_server, start_link, []},
 	    temporary, 1000, worker, [game_server]}
 	  ]}}.
 
 start_socket() ->
     supervisor:start_child(?MODULE, []).
  
-available_listeners() ->
-    [start_socket() || _ <- lists:seq(1,20)],
-    ok.
