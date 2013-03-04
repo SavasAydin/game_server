19a68cc74c25b9809f0993d142664deb503c4131
Savas Aydin <esavayd@esekiws5153.rnd.ki.sw.ericsson.se>
2013-03-04 19:27:34 +0100

    blocking tcp server using gen_server

--- src/game_server.erl
+++ src/game_server.erl
@@ -1,7 +1,9 @@
 -module(game_server).
 -behaviour(gen_server).
 
--export([start_link/1]).
+-export([start_link/0,
+	 stop/0
+	]).
 -export([init/1, 
 	 handle_call/3,
 	 handle_cast/2,
@@ -10,25 +12,43 @@
 	 terminate/2
 	]).
 
--record(state, {socket}). 
+-define(PORT, 8080).
+-define(TCP_OPTIONS, [{active, once}, {packet, 0}]).
 
-start_link(LitenSocket) ->
-    gen_server:start_link(?MODULE, LitenSocket, []).
- 
-init(ListenSocket) ->
+-record(state, {lsocket,
+		asocket,
+		socket,
+		msg
+	       }). 
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:cast(?MODULE, stop).
+
+init([]) ->
+    {ok, ListenSocket} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
     gen_server:cast(self(), accept),
-    {ok, #state{socket = ListenSocket}}.
+    {ok, #state{lsocket = ListenSocket}}.
 
-handle_cast(accept, ListenSocket) ->
-    {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
+handle_cast(accept, State) ->
+    {ok, AcceptSocket} = gen_tcp:accept(State#state.lsocket),
     game_server_sup:start_socket(), 
-    {noreply, #state{socket = AcceptSocket}}.
+    {noreply, #state{asocket = AcceptSocket}}.
 
 handle_call(_E, _From, State) ->
-    {noreply, State}.
+    {reply, ok, State}.
 
-handle_info(_, S) ->
-    {noreply, S}.
+handle_info({tcp, Socket, Data}, State) ->
+    send(Socket, Data),
+    {noreply, State#state{socket = Socket, msg = Data}};
+handle_info({tcp_closed, _Socket, _}, State) ->
+    {stop, normal, State};
+handle_info({tcp_error, _Socket, _}, State) ->
+    {stop, normal, State};
+handle_info(_, State) ->
+    {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -36,5 +56,6 @@ code_change(_OldVsn, State, _Extra) ->
 terminate(normal, _State) ->
     ok.
 
-
+send(Sock, Data) ->
+    gen_tcp:send(Sock, Data).
 
