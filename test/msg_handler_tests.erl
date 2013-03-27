1d02e1d3f65a77fe4ca8f14861a1c43707e70332
savas <savasaydin@gmail.com>
2013-03-27 23:29:48 +0100

    added register_account/0 test as well as setup and teardown functions; setup/0, cleanup/1.

--- test/msg_handler_tests.erl
+++ test/msg_handler_tests.erl
@@ -2,7 +2,29 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-msg_handler_gen_server_start_stop_test() ->
-    ?assertMatch({ok, _}, msg_handler:start_link()),
-    ?assertEqual(ok, msg_handler:stop()).
+-include("include/account.hrl").
+
+msg_handler_test_() ->
+    {setup,
+     fun setup/0,
+     fun cleanup/1,
+     [
+      fun register_account/0
+     ]}.
+
+register_account() ->
+    Savas = create_account(savas, pass),
+    ?assertEqual(ok, msg_handler:register(Savas)).
+
+setup() ->
+    ok = mnesia:start(),
+    {ok, Pid} = msg_handler:start_link(),
+    Pid.
+
+cleanup(_Pid) ->
+    mnesia:stop(),
+    ok = msg_handler:stop().
+
+create_account(Name, Password) ->
+    #account{name = Name, password = Password}.
 
