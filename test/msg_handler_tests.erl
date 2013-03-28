f61683d1029b46f9e8e08fcda6f4a40e847ac1b1
savas <savasaydin@gmail.com>
2013-03-29 00:19:31 +0100

    Splited rather bigger tests into smaller cases and refactored to reduce duplication.

--- test/msg_handler_tests.erl
+++ test/msg_handler_tests.erl
@@ -9,21 +9,32 @@ msg_handler_test_() ->
      fun setup/0,
      fun cleanup/1,
      [
-      fun register_account/0,
-      fun deregister_account/0
+      {"registers non existing account, returns ok",
+       perform_commands(ok, register_account, savas())},
+      {"lists account table, returns [Savas]",
+       perform_commands([savas()], get_accounts, no_args)},
+      {"registers existing name with different password, returns error",
+       perform_commands({error, already_exist}, register_account, savas_different_password())},
+      {"lists account table, returns [Savas]",
+       perform_commands([savas()], get_accounts, no_args)},
+      {"deregisters existing account, returns ok",
+       perform_commands(ok, deregister_account, savas())},
+      {"lists account table, returns empty list",
+       perform_commands([], get_accounts, no_args)},
+      {"deregisters non existing account, returns error",
+       perform_commands({error, not_exist}, deregister_account, savas())},
+      {"lists account table, returns empty list",
+       perform_commands([], get_accounts, no_args)}
      ]}.
 
-register_account() ->
-    Savas = create_account(savas, pass),
-    [] =  msg_handler:get_accounts(),
-    ?assertEqual(ok, msg_handler:register_account(Savas)),
-    ?assertEqual([Savas], msg_handler:get_accounts()).
-
-deregister_account() ->
-    Savas = create_account(savas, pass),
-    [Savas] =  msg_handler:get_accounts(),
-    ?assertEqual(ok, msg_handler:deregister_account(Savas)),
-    [] =  msg_handler:get_accounts().
+perform_commands(ReturnValue, Fun, no_args) ->
+    fun() ->
+	    ?assertEqual(ReturnValue, msg_handler:Fun())
+    end;
+perform_commands(ReturnValue, Fun, Args) ->
+    fun() ->
+	    ?assertEqual(ReturnValue, msg_handler:Fun(Args))
+    end.
 
 setup() ->
     ok = mnesia:start(),
@@ -34,6 +45,8 @@ cleanup(_Pid) ->
     mnesia:stop(),
     ok = msg_handler:stop().
 
-create_account(Name, Password) ->
-    #account{name = Name, password = Password}.
+savas() ->
+    #account{name = savas, password = pass}.
 
+savas_different_password() ->
+    #account{name = savas, password = another_pass}.
