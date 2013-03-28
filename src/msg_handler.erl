435f42d57bd9421abf74fd739ed50c69c8364d7c
savas <savasaydin@gmail.com>
2013-03-29 00:56:33 +0100

    Added get_account/1 and related unit tests, now finds account by name or returns error.

--- src/msg_handler.erl
+++ src/msg_handler.erl
@@ -7,6 +7,7 @@
 -export([start_link/0, 
 	 register_account/1,
 	 get_accounts/0,
+	 get_account/1,
 	 deregister_account/1,
 	 stop/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -19,6 +20,8 @@ start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 register_account(Account) -> gen_server:call(?MODULE, {register, Account}).
 
 get_accounts() -> gen_server:call(?MODULE, get_accounts).
+
+get_account(AccountName) -> gen_server:call(?MODULE, {get_account, AccountName}).
 
 deregister_account(Account) -> gen_server:call(?MODULE, {deregister, Account}).
     
@@ -36,16 +39,13 @@ handle_call({register, Account}, _From, State) ->
     {reply, db_commands:insert(Account, account), State#state{accounts=db_commands:db_to_list()}};
 handle_call(get_accounts, _From, State) ->
     {reply, State#state.accounts, State};
+handle_call({get_account, AccountName}, _From, State) ->
+    {reply, db_commands:get_account(AccountName, account), State};
 handle_call({deregister, Account}, _From, State) ->
-    {reply, db_commands:delete(Account, account), State#state{accounts=db_commands:db_to_list()}};
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+    {reply, db_commands:delete(Account, account), State#state{accounts=db_commands:db_to_list()}}.
 
 handle_cast(stop, State) ->
-    {stop, normal, State};
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+    {stop, normal, State}.
 
 handle_info(_Info, State) ->
     {noreply, State}.
--- test/msg_handler_tests.erl
+++ test/msg_handler_tests.erl
@@ -15,16 +15,26 @@ msg_handler_test_() ->
        perform_commands([savas()], get_accounts, no_args)},
       {"registers existing name with different password, returns error",
        perform_commands({error, already_exist}, register_account, savas_different_password())},
-      {"lists account table, returns [Savas]",
+      {"lists accounts after re-register with same name, returns [Savas]",
        perform_commands([savas()], get_accounts, no_args)},
+      {"registers another non existing account, returns ok",
+       perform_commands(ok, register_account, gianfranco())},
+      {"lists account table, returns [Savas, Gianfranco]",
+       perform_commands([savas(), gianfranco()], get_accounts, no_args)},
       {"deregisters existing account, returns ok",
+       perform_commands(ok, deregister_account, gianfranco())},
+      {"lists accounts after Gianfranco is removed, returns [Savas]",
+       perform_commands([savas()], get_accounts, no_args)},
+      {"deregisters non existing account, returns error",
+       perform_commands({error, not_exist}, deregister_account, simon())},
+      {"gets existing account by name, returns [Savas]",
+       perform_commands([savas()], get_account, savas)},
+      {"deregisters Savas, returns ok",
        perform_commands(ok, deregister_account, savas())},
       {"lists account table, returns empty list",
        perform_commands([], get_accounts, no_args)},
-      {"deregisters non existing account, returns error",
-       perform_commands({error, not_exist}, deregister_account, savas())},
-      {"lists account table, returns empty list",
-       perform_commands([], get_accounts, no_args)}
+      {"gets non existing account by name, returns error",
+       perform_commands({error, not_exist}, get_account, savas)}
      ]}.
 
 perform_commands(ReturnValue, Fun, no_args) ->
@@ -50,3 +60,9 @@ savas() ->
 
 savas_different_password() ->
     #account{name = savas, password = another_pass}.
+
+gianfranco() ->
+    #account{name = gianfranco, password = pass}.
+
+simon() ->
+    #account{name = simon, password = pass}.
