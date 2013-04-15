6da26a0e4e1ba89dfcd3068c0651626adfe64900
savas <savasaydin@gmail.com>
2013-04-15 23:04:08 +0200

    Altered existing test and now tests only whether or not connection accepted

--- test/tcp_server_tests.erl
+++ test/tcp_server_tests.erl
@@ -8,22 +8,16 @@ receive_message_send_ack_test_() ->
      fun setup/0,
      fun cleanup/1,
      [
-      fun send_and_receive_hello/0
+      fun connect_to_tcp_server/0
      ]
     }.
 
-send_and_receive_hello() ->
-    {ok, Sock} = connect(8080, [{active,false},{packet,2}]),
-    ok = send(Sock, "hello"),
-    ?assertEqual("hello", recv(Sock)),
+connect_to_tcp_server() ->
+    ?assertEqual(ok, assert_tcp_server_is_listenning(8080)).
+
+assert_tcp_server_is_listenning(Port) ->
+    {ok, Sock} = connect(Port, [{active,false},{packet,2}]),
     ok = close(Sock).
-
-send(Sock, Msg) ->    
-    gen_tcp:send(Sock, Msg).
-
-recv(Sock) ->
-    {ok, A} = gen_tcp:recv(Sock, 0),
-    A.
 
 close(Sock) ->
     gen_tcp:close(Sock).
@@ -32,11 +26,11 @@ connect(Port, TcpOptions) ->
     gen_tcp:connect("localhost", Port, TcpOptions).
 
 setup() ->
-    {ok, Pid} = tcp_server:start_link(),
+    {ok, Pid} = tcp_server:start_link({message_handler, start_handler}),
     Pid.
 
-cleanup(Pid) ->
-    tcp_server:stop(Pid).
+cleanup(_) ->
+    tcp_server:stop().
 
 
 
